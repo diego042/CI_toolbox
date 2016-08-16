b3582bfba70a24df3e62fd310d9c56c3ee3469ba
Diego Geronimo <diego.macger@hotmail.com>
2016-08-16 16:54:21 -0300

    Update Plot_maps.R
    Correção na importação do csv e conversão de data.frame para "character list"

--- Plot_maps.R
+++ Plot_maps.R
@@ -1,25 +1,40 @@
 # Plot maps from lat/lon data or location names
-# Originaly described by: https://rpubs.com/ldeassis/27602
+# Originaly described by:
+#http://www.r-bloggers.com/r-beginners-plotting-locations-on-to-a-world-map/
+#https://www.students.ncl.ac.uk/keith.newman/r/maps-in-r#countries
 
 #Install and load libraries
-install.packages("pacman")
-pacman::p_load("knitr","knitr","RgoogleMaps","ggplot2","ggmap")
+if (require("pacman")==FALSE) install.packages("pacman")
+pacman::p_load("ggmap", "maptools","maps","mapdata")
 
-# Working with location names
-cidades <- c("Curitiba PR", "São Paulo SP", "Manaus AM")
-DF <- data.frame(cidade=cidades, lat=NA, lon=NA)
-DF <- with(DF,data.frame(cidade=cidade, t(sapply(DF$cidade, getGeoCode))))
-knitr::kable(DF, format="markdown", digits=4)
-box <- make_bbox(lon, lat, data = DF)
-map <-ggmap(get_map(force = T,zoom = 2, color = "color", location=box, 
-        maptype = "terrain", language = "PT")) + geom_point(data=DF, x=DF$lon, y=DF$lat, color="red")
-map
+#Example for testing
+#visited <- c("SFO", "Chennai", "London", "Melbourne", "Johannesbury, SA")
 
-# Working with lat/lon data
-DF <- read.csv("~/coords.csv",sep=";")
-box <- make_bbox(lon, lat, data = DF)
-map <-ggmap(get_map(force = T,zoom = 2, color = "color", location=box, 
-        maptype = "terrain", language = "PT")) + geom_point(data=DF, x=DF$lon, y=DF$lat, color="red")
-map
+# Get locations names from a .csv file
+visited <- read.csv("locais.csv", head=F)
+visited<-as.character(visited[c(1:dim(visited)[1]),])
+ll.visited <- geocode(visited)
+visit.x <- ll.visited$lon
+visit.y <- ll.visited$lat
+dput(visit.x)
+dput(visit.y)
+
+#USING MAPS
+map("world", fill=TRUE, col="white", bg="lightblue", 
+    ylim=c(-60, 90), mar=c(0,0,0,0))
+points(visit.x,visit.y, col="red", pch=16)
+
+#USING MAPS BRAZIL
+map('worldHires',c("brazil"),col=c("gray"), fill=T)
+points(visit.x,visit.y, col="red", pch=16)
+
+#Using GGPLOT, plot the Base World Map
+mp <- NULL
+mapWorld <- borders("world", colour="gray50", 
+                    fill="gray50") # create a layer of borders
+mp <- ggplot() + mapWorld
+#Now Layer the cities on top
+mp <- mp+ geom_point(aes(x=visit.x, y=visit.y), color="blue", size=3) 
+mp
 
 #end
